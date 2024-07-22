660ab011dfe9ec45badc45750bb1d1f6bcbf0020
ali-consciousroad <ali_dindar@live.be>
2024-07-22 15:37:12 +0200

    Improve the code and make it more readable

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dynamicEvents
+
+Guessing game
+
+Find the right number between 1 and 3 to win.
--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 function App() {
+
+    // handleClick event handler
     const handleClick = () => { 
+    // Assign a random number integer between 1 and 3 included to randomNum.  
     let randomNum = Math.floor(Math.random() * 3) + 1;
     console.log(randomNum);
     let userInput = prompt('type a number'); 
-    if (randomNum == userInput) { alert("Congrats, good guess!") };
-    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);  
+    if (randomNum == userInput) { 
+      alert("Congrats, good guess!") }
+    else ( 
+      alert(`Computer number: ${randomNum}, Your guess: ${userInput}`));
   }
+
   return (
     <div>
         <h1>Task: Add a button and handle a click event</h1>
+      {/* Add a React onClick event */}
       <button onClick={handleClick}>
         Guess the number between 1 and 3.   
       </button>
