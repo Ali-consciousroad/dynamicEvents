213d1644fc0be29ff1839fb4b9435e56145716ae
ali-consciousroad <ali_dindar@live.be>
2024-07-23 14:05:52 +0200

    Minor fix + comments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
     const handleClick = () => { 
     // Assign a random number integer between 1 and 3 included to randomNum.  
     let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
+    // Display the random number in the console
+    // console.log(randomNum);
     let userInput = prompt('type a number'); 
     if (randomNum == userInput) { 
       alert("Congrats, good guess!") }
@@ -16,6 +17,8 @@ function App() {
     <div>
         <h1>Task: Add a button and handle a click event</h1>
       {/* Add a React onClick event */}
+      {/* Note that compared with HTML event, 
+      React event handler are written using camelCase */}
       <button onClick={handleClick}>
         Guess the number between 1 and 3.   
       </button>
